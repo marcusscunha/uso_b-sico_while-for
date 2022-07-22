8c9a7e546fd0236526ca30b7ade0c812a3de21fd
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2022-07-22 15:55:54 -0300

    Add files via upload

---
+++ cooldown.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+public class cooldown {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=10;i>=0;i--) {
+			System.out.println("Cooldown: "+i);
+			
+		}
+
+	}
+
+}
